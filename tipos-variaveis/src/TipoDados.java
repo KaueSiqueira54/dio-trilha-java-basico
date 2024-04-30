bad4544e4b0b21ed971635203719b77a3d9e78b2
KaueSiqueira <kaue54150@gmail.com>
2024-04-30 20:44:27 -0300

    tipos_dados_java

---
+++ tipos-variaveis/src/TipoDados.java
@@ -0,0 +1,14 @@
+public class TipoDados {
+    public static void main(String[] args){
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 25698745;
+        long cpf = 65498712368L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+        short numeroCurto = 1;
+        final int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+         
+    }
+}
