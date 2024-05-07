301685c4ecb3d9eba156312f0b8cd575f56d0f6b
KaueSiqueira <kaue54150@gmail.com>
2024-05-07 17:38:22 -0300

    comentarios

---
+++ tipos-variaveis/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora<h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:<b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author kaue7
+ * @version 1.0
+ * @since 07/05/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro de método
+     * @param numeroDois este é o segundo parâmetro de método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+   
+}
---
+++ tipos-variaveis/src/Comentarios.java
@@ -0,0 +1,14 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Sou um comentário de uma única linha
+        /*Olá
+         * Sou um comentário mais detalhado quando necessario
+         */
+    }
+    /** 
+     * Comentário a nivel de documentação
+    */
+    public void metodo(){
+        
+    }
+}
