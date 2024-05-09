ca097e360647afeefc28cc25be8711db26f471cb
KaueSiqueira <kaue54150@gmail.com>
2024-05-09 17:05:55 -0300

    att

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,19 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 4;
+        /*int nota = 8;
 
-        if(nota >= 7)
+        if(nota >= 7) 
             System.out.println("Aprovado");
+        
+        else if (nota >= 5 && nota <7)
+            System.out.println("Recuperação");
         else
             System.out.println("Reprovado");
-    }
+/* */
+        int nota = 2;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "reprovado";
+        System.out.println(resultado);
+        }
+        
+    
 }
