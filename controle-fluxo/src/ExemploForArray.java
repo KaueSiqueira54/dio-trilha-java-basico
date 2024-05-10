ddf2b55620ff3ba4490b3b9778ffc720c0f7e819
KaueSiqueira <kaue54150@gmail.com>
2024-05-09 21:27:59 -0300

    resolvendo

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        //em arrays o indice de elementos inicia em 0
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+        
+    }
+}
