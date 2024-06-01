9001cc8e2108dd1a5b387e6118a58c2855b72095
KaueSiqueira <kaue54150@gmail.com>
2024-05-31 21:22:49 -0300

    agenda-map-exercicio

---
+++ Collections-Java/src/exercicios_map/AgendaContatos.java
@@ -0,0 +1,43 @@
+package exercicios_map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    //atributos
+
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+    
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato (String nome) {
+        if (!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContato() {
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome (String nome) {
+        Integer numeroPorNome = null;
+        if (!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Kaue", 12354);
+        agendaContatos.adicionarContato("Matheus", 65842);
+        agendaContatos.exibirContato();
+    }
+}
