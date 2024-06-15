35a37302b0243dbb91cb95d01f5f438b8325c60c
KaueSiqueira <kaue54150@gmail.com>
2024-06-15 20:16:09 -0300

    exceptions-handler

---
+++ my-first-web-api/src/main/java/dio/web/api/handler/ResponseError.java
@@ -0,0 +1,36 @@
+package dio.web.api.handler;
+
+import java.util.Date;
+
+public class ResponseError {
+    private Date timesTamp = new Date();
+    private String status = "error";
+    private int statusCode = 400;
+    private String error;
+    public Date getTimesTamp() {
+        return timesTamp;
+    }
+    public void setTimesTamp(Date timesTamp) {
+        this.timesTamp = timesTamp;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+    public String getError() {
+        return error;
+    }
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    
+}
