f25ca577a68b1517cb298e3e0d4e5f390c7c125f
KaueSiqueira <kaue54150@gmail.com>
2024-06-17 16:09:39 -0300

    web-security

---
+++ dio-spring-boot-security/src/main/java/dio/spring/security/DioSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+ package dio.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringBootSecurityApplication.class, args);
+	}
+
+}
