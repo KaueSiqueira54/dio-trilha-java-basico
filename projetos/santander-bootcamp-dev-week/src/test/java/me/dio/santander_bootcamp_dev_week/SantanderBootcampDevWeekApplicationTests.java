1bdfc504886fdb8db60b63e91d2bab5e42abd371
KaueSiqueira <kaue54150@gmail.com>
2024-06-19 13:43:37 -0300

    configurando projeto

---
+++ projetos/santander-bootcamp-dev-week/src/test/java/me/dio/santander_bootcamp_dev_week/SantanderBootcampDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
